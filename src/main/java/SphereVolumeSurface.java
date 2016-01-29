e5c2c8e0cd479207d36a8033ff522e0a9c204cc4
Yurim Jeong <min_wooone@naver.com>
2016-01-29 14:06:14 +0900

    Sphere complete

--- src/main/java/SphereVolumeSurface.java
+++ src/main/java/SphereVolumeSurface.java
@@ -6,10 +6,10 @@ public class SphereVolumeSurface{
         System.out.println("Volume: "+results[0]+" Suface: "+results[1]);
     }
 
-    double[] volumeSurface (int r){
+    static double[] volumeSurface (int r){
         double[] results = new double[2];
         results[0] = 4/3*r*r*r*Math.PI;
-        results[1] = 4*Math.PI.r*r;
+        results[1] = 4*Math.PI*r*r;
         return results;
     }
 }
